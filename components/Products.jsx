b5ee7e99e6d05a26eaf79735806707d4cde3e644
Abdallah <bedohossam535@gmail.com>
2023-05-02 14:08:46 +0300

    aa

--- components/Products.jsx
+++ components/Products.jsx
@@ -7,7 +7,6 @@ import "aos/dist/aos.css";
 
 const Products = () => {
   const products = useSelector((state) => state.category.products);
-  console.log(products);
   const [loadMore, setLoadMore] = useState(false);
   const [btndisplay, setBtnDisplay] = useState(true);
   const [spinner, setSpinner] = useState(true);
